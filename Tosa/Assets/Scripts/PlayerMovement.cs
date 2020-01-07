7b0d8115275385522b8a4c40c712038f42f719b3
SaschaLK <SaschaL.Kufahl@gmail.com>
2020-01-07 22:56:45 +0100

    player movement plus jump

---
+++ Tosa/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+
+    private float horizontalMovement;
+    private bool jump = false;
+
+    private void Update() {
+        horizontalMovement = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate() {
+        controller.Move(horizontalMovement * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
